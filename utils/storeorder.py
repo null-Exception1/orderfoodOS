2f09a034f4287a64e39dfc6047e5ee44d5a168e5
null-Exception1 <82306174+null-Exception1@users.noreply.github.com>
2021-10-24 17:28:47 +0530

    Update storeorder.py

--- utils/storeorder.py
+++ utils/storeorder.py
@@ -1,9 +1,6 @@
 import csv
 
 def storeorder(order):
-    with open('orders.csv', 'w', newline='\n') as csvfile:
-        fieldnames = ['Serial No.', 'FoodName','DrinkName']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames,delimiter=',')
-        writer.writeheader()
-        writer.writerow({'Serial No.': order[0], 'FoodName': order[1],'DrinkName' : order[2]})
-        
+    f = open('orders.csv','a+')
+    f.write(order[0]+","+order[1]+'\n')
+    f.close()
